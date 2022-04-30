351ec4ad7a2947fc4b9d3358cab99e573d824c36
Pgerardocastaneda <68565812+Pgerardocastaneda@users.noreply.github.com>
2022-04-29 18:22:42 -0700

    Add files via upload

--- MarkdownParse3.java
+++ MarkdownParse3.java
@@ -5,34 +5,29 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-public class MarkdownParse {
+public class MarkdownParse3 {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int exclamation = markdown.indexOf("!",currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            int openCarrot = markdown.indexOf("<", currentIndex);
-            int closeCarrot = markdown.indexOf(">", openCarrot);
-            if (openBracket != -1 && openParen != -1 && exclamation != openBracket -1) {
+            if (closeBracket != -1 && openBracket != -1) {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
                 currentIndex = closeParen + 1;
             }
-            else if (openCarrot != -1) {
-                toReturn.add(markdown.substring(openCarrot+1, closeCarrot));
-                currentIndex = closeCarrot + 1;
-            }
             else {
-                currentIndex  = markdown.length();
+                currentIndex = markdown.length();
             }
         }
+
         return toReturn;
     }
+
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
